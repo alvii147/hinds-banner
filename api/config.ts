166a21d641ee401acd0d1f94f3de13e9a2b0492d
alvii147 <alvii147@gmail.com>
2024-11-03 15:14:11 -0500

    add gradient

--- api/config.ts
+++ api/config.ts
@@ -2,7 +2,8 @@ export const PORT: number = 3000;
 export const PALESTINE_DATASETS_API_SUMMARY_URL: string = 'https://data.techforpalestine.org/api/v3/summary.min.json';
 
 export interface ColorOptions {
-    colorBackground: string;
+    colorBackgroundPrimary: string;
+    colorBackgroundSecondary: string;
     colorTextBackground: string;
     colorLight: string;
     colorText: string;
@@ -14,7 +15,8 @@ export type Variant = 'classic' | 'crimson' | 'forest' | 'graphite' | 'olive' | 
 
 export const VARIANT_COLOR_OPTIONS: Record<Variant, ColorOptions> = {
     'classic': {
-        colorBackground: 'FFFFFF',
+        colorBackgroundPrimary: 'FFFFFF',
+        colorBackgroundSecondary: 'E6E6E6',
         colorTextBackground: 'FFFFFF',
         colorLight: '737373',
         colorText: '333333',
@@ -22,7 +24,8 @@ export const VARIANT_COLOR_OPTIONS: Record<Variant, ColorOptions> = {
         colorBlood: 'FF1A1A',
     },
     'crimson': {
-        colorBackground: 'FFF7F0',
+        colorBackgroundPrimary: 'FFF7F0',
+        colorBackgroundSecondary: 'FFCCCC',
         colorTextBackground: 'FFF7F0',
         colorLight: 'FF8080',
         colorText: '660000',
@@ -30,7 +33,8 @@ export const VARIANT_COLOR_OPTIONS: Record<Variant, ColorOptions> = {
         colorBlood: 'FF1A1A',
     },
     'forest': {
-        colorBackground: '001A00',
+        colorBackgroundPrimary: '001A00',
+        colorBackgroundSecondary: '1F4D00',
         colorTextBackground: '003300',
         colorLight: '008000',
         colorText: '99FF99',
@@ -38,7 +42,8 @@ export const VARIANT_COLOR_OPTIONS: Record<Variant, ColorOptions> = {
         colorBlood: 'FF1A1A',
     },
     'graphite': {
-        colorBackground: '0D0D0D',
+        colorBackgroundPrimary: '0D0D0D',
+        colorBackgroundSecondary: '4D4D4D',
         colorTextBackground: '404040',
         colorLight: '666666',
         colorText: 'F2F2F2',
@@ -46,7 +51,8 @@ export const VARIANT_COLOR_OPTIONS: Record<Variant, ColorOptions> = {
         colorBlood: 'FF1A1A',
     },
     'olive': {
-        colorBackground: '666633',
+        colorBackgroundPrimary: '666633',
+        colorBackgroundSecondary: '446600',
         colorTextBackground: 'CCCC99',
         colorLight: '1A1A1A',
         colorText: '333333',
@@ -54,7 +60,8 @@ export const VARIANT_COLOR_OPTIONS: Record<Variant, ColorOptions> = {
         colorBlood: 'FF1A1A',
     },
     'sand': {
-        colorBackground: '997300',
+        colorBackgroundPrimary: '997300',
+        colorBackgroundSecondary: '996100',
         colorTextBackground: 'FFD966',
         colorLight: '1A1A1A',
         colorText: '333333',
@@ -62,7 +69,8 @@ export const VARIANT_COLOR_OPTIONS: Record<Variant, ColorOptions> = {
         colorBlood: 'FF1A1A',
     },
     'tatreez': {
-        colorBackground: '1A0A00',
+        colorBackgroundPrimary: '1A0A00',
+        colorBackgroundSecondary: '662700',
         colorTextBackground: '4D1F00',
         colorLight: 'CC5200',
         colorText: 'FFB380',
